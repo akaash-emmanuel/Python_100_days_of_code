8eb48b7e21d957558dfd93cda7872e327152e61a
Akaash <117347573+akaash-emmanuel@users.noreply.github.com>
2024-06-17 10:44:12 +0530

    day 1

---
+++ day1/practice.py
@@ -0,0 +1,16 @@
+print("something")                                  # print function syntax in python
+
+print('She said: "Hello" and then left.')           # by default use '' as the quotes
+
+print('She said: \"hello\" and then left')          # can use \"....\" to do the same
+
+print(input("question:"))                           # to ask for input
+
+print(input())                                      # no prompt question to take inputs
+
+var = int(input())                                  #storing an input into a variable 'var' which is an integer
+
+len("string")                                       # a length string to find out the number of letters in the string
+
+print("this is a: " + var)                          # similar to java, use '+' to add a variable to the string while printing
+
---
+++ day1/projectday1.py
@@ -0,0 +1,4 @@
+# band name generator
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pets name?\n")
+print("Your band name is: " + city + " " + pet)
