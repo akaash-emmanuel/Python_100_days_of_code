ed3052d40ccc1eb64c3d99889b79a62df5bb0bbd
Akaash <117347573+akaash-emmanuel@users.noreply.github.com>
2024-06-28 23:02:29 +0530

    day6late

---
+++ day6/practice.py
@@ -0,0 +1,17 @@
+#functions
+
+# def function_name():   is how you define a function, the code in the code block will be what the function does
+# function_name()      this is how you call a function.
+# 1. define the function   2. the functionality of the function   3. calling the function
+# link: "reeborg's world" for practice
+# for indentation, use spaces instead of tabs because its the preferred indentation choice in python
+
+
+#while loops:
+#while something_is_true:
+        # code that is repeated
+#this makes so that when that something is false, the loop stops and when its true, it keeps running
+
+
+
+
