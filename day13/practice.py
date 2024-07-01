48abee55b7efbf2b159ecd431be399c21bda51f9
Akaash <117347573+akaash-emmanuel@users.noreply.github.com>
2024-07-01 12:07:38 +0530

    day14

---
+++ day13/practice.py
@@ -0,0 +1,6 @@
+# debugging steps :   1. untangle the problem, i.e describe the problem
+#                     2. reproduce the bug, i.e if a bug occurs a few times but doesn't usually, then try to make the bug happen to fix it
+#                     3. play computer and evaluate each line
+#                     4. use print() statement to fix the errors
+#                     5. use a debugger
+
