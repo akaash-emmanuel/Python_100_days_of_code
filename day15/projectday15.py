260d322d29e1f7531dec1785ceed4ca277028e9f
Akaash <117347573+akaash-emmanuel@users.noreply.github.com>
2024-07-01 12:32:16 +0530

    day15

---
+++ day15/projectday15.py
@@ -0,0 +1,87 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+###################################################################################
+profit = 0
+is_on = True
+
+def sufficient(order_ingredients):
+    is_enough = True
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            is_enough = False
+    return is_enough
+
+def coins():
+    print("Please insert coins")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+def success(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is the change: ${change}")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry, not enough money. Money refunded")
+        return False
+    
+def make_drink(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}")
+    
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappucino): ")
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if sufficient(drink['order_ingredients']):
+            payment = coins()
+            if success(payment, drink["cost"]):
+                make_drink(choice, drink["ingredients"])
+
+
+        
+
